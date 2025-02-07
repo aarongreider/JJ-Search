ea64497668633e678463ca418d1d7e18006d7882
Aaron Greider <40152187+aarongreider@users.noreply.github.com>
2025-02-07 10:41:09 -0500

    override browser default styles

--- src/components/Search Controls/SearchControls.tsx
+++ src/components/Search Controls/SearchControls.tsx
@@ -21,7 +21,7 @@ export default function SearchControls({ searchParams, searchResults, totalServe
     const [departments, setDepartments] = useState<string[]>([])
     const [navHeight, setNavHeight] = useState<number>(50)
     const searchBar = useRef<HTMLInputElement>(null)
-    
+
 
     useEffect(() => {
         setDisplaySuggestors(false)
@@ -34,7 +34,7 @@ export default function SearchControls({ searchParams, searchResults, totalServe
         // evaluate if the current department value is valid after search results change
         const newDepartments = [...new Set(searchResults.map(item => item.Department))]
         if (!departments.includes(searchParams.dept)) {
-            setSearchParams({...searchParams, dept: ''})
+            setSearchParams({ ...searchParams, dept: '' })
         }
         setDepartments(newDepartments)
     }, [searchResults])
@@ -77,16 +77,16 @@ export default function SearchControls({ searchParams, searchResults, totalServe
 
     useEffect(() => { // window size listener
         const handleResize = () => {
-          setNavHeight(document.getElementById('nav')?.offsetHeight ?? 50)
+            setNavHeight(import.meta.env.PROD ? document.getElementById('nav')?.offsetHeight ?? 50 : 0)
         };
-    
+
         handleResize(); // Check on mount
         window.addEventListener('resize', handleResize); // Check on resize
-    
+
         return () => {
-          window.removeEventListener('resize', handleResize); // Cleanup
+            window.removeEventListener('resize', handleResize); // Cleanup
         };
-      }, [window.innerWidth, window.innerHeight]);
+    }, [window.innerWidth, window.innerHeight]);
 
     const selectSuggestor = (suggestor: Suggestor) => {
         setSearchParams({ ...searchParams, query: suggestor.Description.toLocaleLowerCase() })
@@ -108,81 +108,96 @@ export default function SearchControls({ searchParams, searchResults, totalServe
         setSearchParams({ ...searchParams, query: suggestor });
         setTriggerSearch(!triggerSearch)
     }
-
+    const sortRef = useRef<HTMLSelectElement>(null)
+    const storeRef = useRef<HTMLSelectElement>(null)
+    const deptRef = useRef<HTMLSelectElement>(null)
 
     return <>
-    {/* SEARCH BAR */}
-            <div className='inputWrapper' style={{top: `${navHeight + 10}px`}}>
-                <div style={{ display: 'flex', alignItems: 'center', gap: '3px', width: '100%', position: 'relative', marginBottom: `${displaySuggestors ? '5px' : 0}` }}>
-                    <span className="material-symbols-outlined" style={{ cursor: 'default' }}>search</span>
-                    <input type="text" ref={searchBar} style={{ background: "none", border: "none", outline: 'none', width: '100%', paddingLeft: '2px' }}
-                        placeholder="Search..."
-                        value={searchParams.query}
-                        onChange={(e) => setSearchParams({ ...searchParams, query: e.target.value, })}
-                        onKeyDown={(e) =>
-                            e.key === "Enter" ?
-                                activeSuggestorIndex < 0 ?
-                                    setTriggerSearch(!triggerSearch)
-                                    : searchSuggestor(suggestors[activeSuggestorIndex].Description.toLowerCase())
-                                : undefined}
-                        onFocus={() => { setDisplaySuggestors(true) }}
-                        onBlur={() => setTimeout(() => { setDisplaySuggestors(false) }, 200)}
-                    />
-                    <span className="material-symbols-outlined" style={{ cursor: 'pointer' }} onClick={() => { setSearchParams({ ...searchParams, query: "" }); setSuggestors([]) }}>close</span>
-                    {displaySuggestors && <hr />}
-                </div>
-                {/* SUGGESTORS */}
-                {displaySuggestors && <ul className='suggestorContainer' style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-                    {displaySuggestors ? suggestors.map((suggestor, index) => {
-                        return <>
-                            <li key={index} className={`${index == activeSuggestorIndex ? 'selected' : ''}`} style={{ display: 'flex', alignItems: 'center', gap: '3px', width: '100%' }} onClick={() => {
-                                selectSuggestor(suggestor)
-                            }}>
-                                <span style={{ color: 'rgb(172, 172, 172)' }} className="material-symbols-outlined">search</span>
-                                <button key={index} className="suggestor" style={{ whiteSpace: "nowrap" }}
-                                    dangerouslySetInnerHTML={{
-                                        __html: suggestor['@search.text'],
-                                    }} />
-                                <span className='suggestorBrand'>{suggestor.Brand.toLocaleLowerCase()}</span>
-                            </li>
-                        </>
-                    }) : undefined
-                    }
-                </ul>}
+        {/* SEARCH BAR */}
+        <div className='inputWrapper' style={{ top: `${navHeight + 10}px` }}>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '3px', width: '100%', position: 'relative', marginBottom: `${displaySuggestors ? '5px' : 0}` }}>
+                <span className="material-symbols-outlined" style={{ cursor: 'default' }}>search</span>
+                <input type="text" ref={searchBar} style={{ background: "none", border: "none", outline: 'none', width: '100%', paddingLeft: '2px' }}
+                    placeholder="Search..."
+                    value={searchParams.query}
+                    onChange={(e) => setSearchParams({ ...searchParams, query: e.target.value, })}
+                    onKeyDown={(e) =>
+                        e.key === "Enter" ?
+                            activeSuggestorIndex < 0 ?
+                                setTriggerSearch(!triggerSearch)
+                                : searchSuggestor(suggestors[activeSuggestorIndex].Description.toLowerCase())
+                            : undefined}
+                    onFocus={() => { setDisplaySuggestors(true) }}
+                    onBlur={() => setTimeout(() => { setDisplaySuggestors(false) }, 200)}
+                />
+                <span className="material-symbols-outlined" style={{ cursor: 'pointer' }} onClick={() => { setSearchParams({ ...searchParams, query: "" }); setSuggestors([]) }}>close</span>
+                {displaySuggestors && <hr />}
             </div>
+            {/* SUGGESTORS */}
+            {displaySuggestors && <ul className='suggestorContainer' style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+                {displaySuggestors ? suggestors.map((suggestor, index) => {
+                    return <>
+                        <li key={index} className={`${index == activeSuggestorIndex ? 'selected' : ''}`} style={{ display: 'flex', alignItems: 'center', gap: '3px', width: '100%' }} onClick={() => {
+                            selectSuggestor(suggestor)
+                        }}>
+                            <span style={{ color: 'rgb(172, 172, 172)' }} className="material-symbols-outlined">search</span>
+                            <button key={index} className="suggestor" style={{ whiteSpace: "nowrap" }}
+                                dangerouslySetInnerHTML={{
+                                    __html: suggestor['@search.text'],
+                                }} />
+                            <span className='suggestorBrand'>{suggestor.Brand.toLocaleLowerCase()}</span>
+                        </li>
+                    </>
+                }) : undefined
+                }
+            </ul>}
+        </div>
         {/* SEARCH CONTROLS */}
         <div id='searchControls'>
 
-            
+
 
             {/* FILTERING */}
             <div id="filterBar" style={{ display: 'flex', flexDirection: 'row', gap: '10px', flexWrap: 'wrap', justifyContent: 'flex-end', }}>
 
                 {/* SORT */}
-                <select className="filterButton" onChange={(e) => { setSearchParams({ ...searchParams, sort: e.currentTarget.value }) }}>
-                    <option value="">Relevance</option>
-                    <option value="price descending">Most $</option>
-                    <option value="price ascending">Least $</option>
-                    <option value="alphabetically">A-Z</option>
-                </select>
+                <div className="filterButton" onClick={() => { sortRef?.current?.showPicker() }}>
+                    <select ref={sortRef} onChange={(e) => { setSearchParams({ ...searchParams, sort: e.currentTarget.value }) }}>
+                        <option value="">Relevance</option>
+                        <option value="price descending">Most $</option>
+                        <option value="price ascending">Least $</option>
+                        <option value="alphabetically">A-Z</option>
+                    </select>
+                    <span className="material-symbols-outlined chevDown">chevron_right</span>
+                </div>
 
                 {/* FILTER STORE */}
-                <select className="filterButton" onChange={(e) => { setSearchParams({ ...searchParams, store: e.currentTarget.value, showOOS: false }) }}>
-                    <option value="">Both Stores</option>
-                    <option value="FF">Fairfield</option>
-                    <option value="EG">Eastgate</option>
-                </select>
+                <div className="filterButton" onClick={() => { storeRef?.current?.showPicker() }}>
+                    <select ref={storeRef} onChange={(e) => { setSearchParams({ ...searchParams, store: e.currentTarget.value, showOOS: false }) }}>
+                        <option value="">Both Stores</option>
+                        <option value="FF">Fairfield</option>
+                        <option value="EG">Eastgate</option>
+                    </select>
+                    <span className="material-symbols-outlined chevDown">chevron_right</span>
+                </div>
 
                 {/* FILTER DEPARTMENT */}
-                <select className="filterButton" value={searchParams.dept} onChange={(e) => { setSearchParams({ ...searchParams, dept: e.currentTarget.value }) }}>
-                    <option value="">All Departments</option>
-                    {departments.map((department, index) => {
-                        return <option key={index} value={department}>{department}</option>
-                    })}
-                </select>
+                <div className="filterButton" onClick={() => { deptRef?.current?.showPicker() }}>
+                    <select ref={deptRef} value={searchParams.dept} onChange={(e) => { setSearchParams({ ...searchParams, dept: e.currentTarget.value }) }}>
+                        <option value="">All Departments</option>
+                        {departments.map((department, index) => {
+                            return <option key={index} value={department}>{department}</option>
+                        })}
+                    </select>
+                    <span className="material-symbols-outlined chevDown">chevron_right</span>
+                </div>
 
                 {/* TOGGLE SOLD OUT */}
-                <div className={`filterButton ${searchParams.store ? "disabled" : ""}`} style={{ display: 'flex', flexDirection: 'row', gap: '10px', flexWrap: 'wrap', alignItems: 'center', justifyContent: "flex-end" }}>
+                <div
+                    className={`filterButton ${searchParams.store ? "disabled" : ""}`}
+                    style={{ display: 'flex', flexDirection: 'row', gap: '10px', flexWrap: 'wrap', alignItems: 'center', justifyContent: "flex-end" }}
+                    onClick={() => setSearchParams({ ...searchParams, showOOS: !searchParams.showOOS })}
+                >
                     <input type='checkbox'
                         disabled={searchParams.store ? true : false}
                         checked={searchParams.showOOS}
